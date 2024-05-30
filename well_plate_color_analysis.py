dcad678b035eff2a2479ba58fd7eaea780240a23
stephaniewang26 <stephanie.wang26@trinityschoolnyc.org>
2024-05-30 12:07:44 -0400

    update 5/30

--- well_plate_color_analysis.py
+++ well_plate_color_analysis.py
@@ -21,27 +21,75 @@ def binary_mask(wellimage):
             ogx+=129
         ogy+=129
 
-    cv2.imwrite("bwmask.png", bwmask)
+    cv2.imwrite("plate_mask.png", bwmask)
     return(bwmask)
 
-def color_mask(bwmask):
+def color_mask(bwmask, wellimage):
     colormask = bwmask.copy()
+    ogimage = wellimage.copy()
 
+    for r, row in enumerate(bwmask):
+        for c, value in enumerate(row):
+            if tuple(colormask[r][c]) != (0,0,0):
+                colormask[r][c] = ogimage[r][c]
+
+    cv2.imwrite("plate_color_grid.png", colormask)
     return(colormask)
+
+def avg_rgb_dilution(colormask):
+    dilutionticks = []
+
+    count = 1
+    for i in range(12):
+        dilutionticks.append(count)
+        count *= 2
+
+    ogx = 75
+    ogy = 73
+    rad = 40
+
+    yellowdict = {
+
+    }
+    reddict = {}
+    bluedict = {}
+    greendict = {}
+    dictlist = [yellowdict,reddict,bluedict,greendict]
+
+    for eachdict in dictlist:
+        for i in range(12):
+            eachdict[i] = dict()
+            eachdict[i]["minx"] = ogx-40
+            eachdict[i]["maxx"] = ogx+40
+            eachdict[i]["miny"] = ogy-40
+            eachdict[i]["maxy"] = ogy+40
+
+            ogx += 129
+            ogy += 129
+
+    print(dictlist)
+
+    
+    
+    print(dilutionticks)
 
 if __name__ == '__main__':
     well_image_location = 'colorplate.png'
     well_image_read = cv2.imread(well_image_location)
 
-    bw_mask_location = "bwmask.png"
-    color_mask_location = "colormask.png"
+    bw_mask_location = "plate_mask.png"
+    color_mask_location = "plate_color_grid.png"
 
     cv2.imshow(f'{well_image_location} - original', well_image_read)
     cv2.imshow(f'{bw_mask_location} - BW mask', binary_mask(well_image_read))  
 
     bw_mask_read = cv2.imread(bw_mask_location)
 
-    cv2.imshow(f'{color_mask_location} - color mask', color_mask(bw_mask_read))  
+    cv2.imshow(f'{color_mask_location} - color mask', color_mask(bw_mask_read, well_image_read))  
+
+    color_mask_read = cv2.imread(color_mask_location)
+
+    avg_rgb_dilution(color_mask_read)
     
     cv2.waitKey() 
     cv2.destroyAllWindows() 
